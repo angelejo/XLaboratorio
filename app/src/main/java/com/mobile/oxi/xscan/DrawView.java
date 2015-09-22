5975591fbfa35dfa51182546e4cb28919e258662
angelejo <angelejo@hotmail.com>
2015-09-22 16:22:46 -0500

    Laboratorio 1

--- app/src/main/java/com/mobile/oxi/xscan/DrawView.java
+++ app/src/main/java/com/mobile/oxi/xscan/DrawView.java
@@ -7,10 +7,16 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.util.Log;
 import android.view.View;
 
 public class DrawView extends View {
     Paint paint = new Paint();
+
+    public float valx=0;
+    public float valy=0;
+    public float ancho=0;
+    public float alto=0;
 
     public DrawView(Context context) {
         super(context);
@@ -20,13 +26,25 @@ public class DrawView extends View {
     public void onDraw(Canvas canvas) {
         paint.setColor(Color.WHITE);
         paint.setStrokeWidth(1);
-        canvas.drawRect(100, 100, 150, 170, paint);
+        //paint.setARGB(255,255,0,0);
+        paint.setStyle(Paint.Style.STROKE);
+
+        canvas.drawRect(valx, valy, ancho, alto, paint);
       //  paint.setStrokeWidth(0);
        // paint.setColor(Color.CYAN);
       //  canvas.drawRect(33, 60, 77, 77, paint );
        // paint.setColor(Color.YELLOW);
        // canvas.drawRect(33, 33, 77, 60, paint );
 
+    }
+
+
+    public void rectVisor (float x,float y, float anc, float alt)
+    {
+        valx=x;
+        valy=y;
+        ancho=anc;
+        alto=alt;
     }
 
 }
